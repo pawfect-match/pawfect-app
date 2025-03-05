ef3e280bb560530c35ead969a33a5512cbfc4f94
Youngjin <yjshin229@gmail.com>
2025-03-05 01:10:50 -0800

    add filterBar

--- src/screens/messages/components/MessageTabFilterBar.tsx
+++ src/screens/messages/components/MessageTabFilterBar.tsx
@@ -1,6 +1,8 @@
-import { Col } from '@design/styleAsProps';
+import { Row, Span } from '@design/styleAsProps';
+import i18n from '@locales';
 import React, { useState, useEffect } from 'react';
-import { SafeAreaView, View, StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { StyleSheet, TouchableOpacity } from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 interface Props {}
 
@@ -23,32 +25,45 @@ const MessageTabFilterBar = () => {
    * life cycles
    **************/
 
-  useEffect(() => {
-    // ComponentDidMount
-
-    // setIsRendering(false)
-    return () => {
-      // ComponentWillUnmount
-    };
-  }, []);
-
   /************
    * functions
    ************/
+
+  const onPressSort = () => {};
 
   /*********
    * render
    *********/
 
-  // if (isRendering === true) {
-  // return null
-  // }
+  const renderSort = () => {
+    return (
+      <TouchableOpacity style={{ alignItems: 'center', flex: 1, flexDirection: 'row' }}>
+        <Ionicons name="filter-circle-outline" size={24} />
+        <Span titleS ml5>
+          {i18n.t('common-sort')}
+        </Span>
+      </TouchableOpacity>
+    );
+  };
+
+  const renderArchived = () => {
+    return (
+      <TouchableOpacity>
+        <Ionicons name="archive-outline" size={24} />
+      </TouchableOpacity>
+    );
+  };
 
   /***********
    * render()
    ***********/
 
-  return <Col h44 bgAlertMajor></Col>;
+  return (
+    <Row ph14 pv12 alignCenter justifyBetween>
+      {renderSort()}
+      {renderArchived()}
+    </Row>
+  );
 };
 
 const styles = StyleSheet.create({});
